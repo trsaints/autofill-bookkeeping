1cda6b9c0766692231946cf0259e8c14941586fa
trsaints <thiago.dev2k3@gmail.com>
2024-03-12 14:49:10 -0300

    move console output responsibility to autofill.py

--- autofill.py
+++ autofill.py
@@ -8,6 +8,8 @@ def fill_form(start_date, end_date, fill_type, dev_mode=False):
     start_date_object = datetime.datetime.strptime(start_date, '%d/%m/%Y')
     end_date_object = datetime.datetime.strptime(end_date, '%d/%m/%Y')
 
+    print(f'Starting to fill form from {start_date} to {end_date}...')
+
     current_date = start_date_object
 
     while current_date <= end_date_object:
@@ -17,27 +19,34 @@ def fill_form(start_date, end_date, fill_type, dev_mode=False):
         print(f'Filling form for {current_date.strftime("%d/%m/%Y")}...')
 
         pyautogui.hotkey('ctrl', 'insert')
+        
         pyautogui.press('tab')
         pyautogui.typewrite(fill_type + ' - ' +
                             current_date.strftime('%d/%m/%Y'))
         pyautogui.press('tab', presses=3)
         pyautogui.press('down')
+
         pyautogui.press('tab', presses=4)
         pyautogui.typewrite('1')
         pyautogui.press('tab', presses=3)
         pyautogui.typewrite('1')
         pyautogui.press('tab', presses=3)
+
         pyautogui.typewrite(current_date.strftime('%d/%m/%Y'))
         pyautogui.press('tab')
         pyautogui.typewrite(current_date.strftime('%d/%m/%Y'))
         pyautogui.press('tab')
+
         pyautogui.typewrite('000000000000000')
         pyautogui.press('tab')
         pyautogui.typewrite('999999999999999')
         pyautogui.press('tab')
+
         if fill_type == 'NFS-e':
             pyautogui.typewrite('2216')
         pyautogui.press('tab', presses=5)
         pyautogui.press('enter')
 
         current_date += relativedelta(days=+1)
+
+    print('Form filling complete.')
--- autofill_bookkeeping.py
+++ autofill_bookkeeping.py
@@ -7,15 +7,11 @@ def initialize():
     args = get_args()
     [start_date, end_date, fill_type] = args
 
-    print('starting automation...')
-
     try:
         switch_window('Sem título - Bloco de Notas')
         fill_form(start_date, end_date, fill_type, False)
     except Exception as e:
         print(f'Failed to fill form due to error: {e.with_traceback()}')
-
-    print('process finished')
 
 
 initialize()
