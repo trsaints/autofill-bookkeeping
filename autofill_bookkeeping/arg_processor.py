ceb52294af2d2de62ef222c143297430e813b499
trsaints <thiago.dev2k3@gmail.com>
2024-03-20 14:14:55 -0300

    add debug flag argument

--- autofill_bookkeeping/arg_processor.py
+++ autofill_bookkeeping/arg_processor.py
@@ -6,12 +6,14 @@ def get_args():
     parser.add_argument('-s', '--start_date', required=True, type=str)
     parser.add_argument('-e', '--end_date', required=True, type=str)
     parser.add_argument('-t', '--fill_type', required=True, type=str)
-    parser.add_argument('-p', '--pid', required=False, type=int)
+    parser.add_argument('-p', '--pid', required=True, type=int)
+    parser.add_argument('-d', '--dev_mode', action='store_true')
 
     args = parser.parse_args()
     start_date = args.start_date
     end_date = args.end_date
     fill_type = args.fill_type
     pid = args.pid
+    dev_mode = args.dev_mode
 
-    return [start_date, end_date, fill_type, pid]
+    return [start_date, end_date, fill_type, pid, dev_mode]
--- autofill_bookkeeping/autofill_bookkeeping.py
+++ autofill_bookkeeping/autofill_bookkeeping.py
@@ -5,11 +5,11 @@ from window_controller import switch_window
 
 def initialize():
     args = get_args()
-    [start_date, end_date, fill_type, pid] = args
+    [start_date, end_date, fill_type, pid, dev_mode] = args
 
     try:
         switch_window(pid)
-        auto_fill(start_date, end_date, fill_type, False)
+        auto_fill(start_date, end_date, fill_type, dev_mode)
     except Exception as e:
         print(f'Failed to fill form due to error: {e.with_traceback()}')
 
